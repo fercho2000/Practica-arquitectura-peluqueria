ad53ec7ba12fcade1b9aaf94ae4c83dfa10a411c
Jose Fernando U <44246337+fercho2000@users.noreply.github.com>
2022-09-22 21:09:44 -0500

    first commit

---
+++ src/main/java/poli/peluqueria/peluqueria/PeluqueriaApplication.java
@@ -0,0 +1,13 @@
+package poli.peluqueria.peluqueria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeluqueriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeluqueriaApplication.class, args);
+	}
+
+}
